e812f33290a71e37c4fd85b6adb2a5ad5a39f711
Luke Lancaster <luketlancaster@gmail.com>
2020-08-26 16:26:30 -0500

    Initial setup

--- src/App/App.js
+++ src/App/App.js
@@ -5,9 +5,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>APP COMPONENT HERE!!!!</h1>
+        <h1><span role="img" aria-label="poop emoji">💩</span> Surprise!</h1>
         <button className="btn btn-info">
-          <i className="fab fa-react"></i> Btn Here <i className="fab fa-react"></i>
+          <i className="fas fa-poo-storm"></i> Btn Here <i className="fas fa-poo-storm"></i>
         </button>
       </div>
     );
