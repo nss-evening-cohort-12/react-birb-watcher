efa75b8e82c09dc7ddef10c4c2869166d416bad3
Luke Lancaster <luketlancaster@gmail.com>
2020-08-29 10:56:13 -0500

    Edit birb route working

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 class Home extends React.Component {
+  editBirbEvent = (e) => {
+    e.preventDefault();
+    const birbId = 'birb10000';
+    this.props.history.push(`/edit/${birbId}`);
+  }
+
   render() {
     return (
       <div className="Home">
         <h1>Home</h1>
+        <button className="btn btn-dark" onClick={this.editBirbEvent}>Edit A Birb</button>
       </div>
     );
   }
