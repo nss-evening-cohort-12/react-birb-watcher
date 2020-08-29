4386c1d3081256b7a64ff9763f5699f8bfdcc065
Luke Lancaster <luketlancaster@gmail.com>
2020-08-29 09:37:10 -0500

    Create base components

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,11 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import Auth from '../components/pages/Auth/Auth';
+import EditBirb from '../components/pages/EditBirb/EditBirb';
+import Home from '../components/pages/Home/Home';
 import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+import NewBirb from '../components/pages/NewBirb/NewBirb';
+import SingleBirb from '../components/pages/SingleBirb/SingleBirb';
 
 import fbConnection from '../helpers/data/connection';
 
@@ -36,6 +40,11 @@ class App extends React.Component {
         <h1><span role="img" aria-label="bird emoji">🦉</span> Watcher!</h1>
         <MyNavbar />
         <Auth />
+
+        <EditBirb />
+        <Home />
+        <NewBirb />
+        <SingleBirb />
       </div>
     );
   }
