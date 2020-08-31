fe853f796e836fae01ad866990c6dcbcad99b298
Luke Lancaster <luketlancaster@gmail.com>
2020-08-31 18:46:04 -0500

    Go get all birbs and show basic BirdCard on Home

---
+++ src/components/shared/BirbCard/BirbCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BirbCard extends React.Component {
+  render() {
+    return (
+      <div className="Bird">
+        <h3>Here is a birb</h3>
+      </div>
+    );
+  }
+}
+
+export default BirbCard;
