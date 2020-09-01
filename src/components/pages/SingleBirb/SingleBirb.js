5834e6ffd0699b2cf0cce04a764f1a203b3a87ec
Luke Lancaster <luketlancaster@gmail.com>
2020-08-31 19:15:23 -0500

    Route to and display single birb view

--- src/components/pages/SingleBirb/SingleBirb.js
+++ src/components/pages/SingleBirb/SingleBirb.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import birbsData from '../../../helpers/data/birbsData';
 
 class SingleBirb extends React.Component {
+  state = {
+    birb: {},
+  }
+
+  componentDidMount() {
+    const { birbId } = this.props.match.params;
+
+    birbsData.getBirbById(birbId)
+      .then((res) => this.setState({ birb: res.data }))
+      .catch((err) => console.error('get single birb failed', err));
+  }
+
   render() {
     return (
       <div className="SingleBirb">
--- src/components/shared/BirbCard/BirbCard.js
+++ src/components/shared/BirbCard/BirbCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import birbShape from '../../../helpers/propz/birbShape';
 
@@ -7,14 +8,25 @@ class BirbCard extends React.Component {
     birb: birbShape.birbShape,
   }
 
+  // be able to go to the SingleBirb view and the EditBirb view for THIS birb
+  // create a link for each of those?
+  // throw those links in the cards?
+  // click on them?
+  // 💸
+
   render() {
     const { birb } = this.props;
+
+    const singleBirbLink = `/birbs/${birb.id}`;
+    const editLink = `/edit/${birb.id}`;
 
     return (
       <div className="Bird card">
         <div className="card-body">
           <h5 className="card-title">{birb.type}</h5>
           <p className="card-text">{birb.notes}</p>
+          <Link to={singleBirbLink} className="btn btn-warning"><i className="fas fa-binoculars"></i></Link>
+          <Link to={editLink} className="btn btn-success"><i className="fas fa-pencil-alt"></i></Link>
         </div>
       </div>
     );
