6f542c22b6f21ab78d1ad0cd4e3b4dc35182725e
Luke Lancaster <luketlancaster@gmail.com>
2020-09-01 10:10:00 -0500

    Pretty up the birb and hook-ify some components

--- src/components/pages/SingleBirb/SingleBirb.js
+++ src/components/pages/SingleBirb/SingleBirb.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+
 import birbsData from '../../../helpers/data/birbsData';
 
-class SingleBirb extends React.Component {
-  state = {
-    birb: {},
-  }
+const SingleBirb = (props) => {
+  const [birb, setBirb] = useState({});
 
-  componentDidMount() {
-    const { birbId } = this.props.match.params;
+  useEffect(() => {
+    const { birbId } = props.match.params;
 
     birbsData.getBirbById(birbId)
-      .then((res) => this.setState({ birb: res.data }))
+      .then((res) => setBirb(res.data))
       .catch((err) => console.error('get single birb failed', err));
-  }
+  }, [props.match.params]);
 
-  render() {
-    return (
-      <div className="SingleBirb">
-        <h1>SingleBirb</h1>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="SingleBirb mt-3 p-3 bg-success rounded">
+      <h1>Welcome to the {birb.type} page</h1>
+      <p>Bird seen at: {birb.location}</p>
+      <p>Size: {birb.size}</p>
+      <p>Colors: {birb.color}, {birb.altColor}</p>
+      <p>Last seen on: {moment(birb.seenAt).format('MMMM Do YYYY, h:mma')}</p>
+      <p>Note: {birb.notes}</p>
+      <p>Was Sleeping: {birb.wasSleeping ? 'Yes' : 'No'}</p>
+    </div>
+  );
+};
 
 export default SingleBirb;
--- src/components/shared/BirbCard/BirbCard.js
+++ src/components/shared/BirbCard/BirbCard.js
@@ -4,39 +4,31 @@ import moment from 'moment';
 
 import birbShape from '../../../helpers/propz/birbShape';
 
-class BirbCard extends React.Component {
-  static propTypes = {
-    birb: birbShape.birbShape,
-  }
+const BirbCard = (props) => {
+  const { birb } = props;
 
-  // be able to go to the SingleBirb view and the EditBirb view for THIS birb
-  // create a link for each of those?
-  // throw those links in the cards?
-  // click on them?
-  // 💸
+  const singleBirbLink = `/birbs/${birb.id}`;
+  const editLink = `/edit/${birb.id}`;
 
-  render() {
-    const { birb } = this.props;
-
-    const singleBirbLink = `/birbs/${birb.id}`;
-    const editLink = `/edit/${birb.id}`;
-
-    return (
-      <div className="col-4 mb-3">
-        <div className="Bird card">
-          <div className="card-body">
-            <h5 className="card-title">{birb.type}</h5>
-            <p className="card-text">{birb.notes}</p>
-            <Link to={singleBirbLink} className="btn btn-warning mr-2"><i className="fas fa-binoculars"></i></Link>
-            <Link to={editLink} className="btn btn-success"><i className="fas fa-pencil-alt"></i></Link>
-          </div>
-          <div className="card-footer text-muted">
-            Updated: {moment(birb.seenAt).fromNow()}
-          </div>
+  return (
+    <div className="col-4 mb-3">
+      <div className="Bird card border-0">
+        <div className="card-body">
+          <h5 className="card-title">{birb.type}</h5>
+          <p className="card-text">{birb.notes}</p>
+          <Link to={singleBirbLink} className="btn btn-warning mr-2"><i className="fas fa-binoculars"></i></Link>
+          <Link to={editLink} className="btn btn-success"><i className="fas fa-pencil-alt"></i></Link>
+        </div>
+        <div className="card-footer text-muted">
+          Updated: {moment(birb.seenAt).fromNow()}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+BirbCard.propTypes = {
+  birb: birbShape.birbShape,
+};
 
 export default BirbCard;
