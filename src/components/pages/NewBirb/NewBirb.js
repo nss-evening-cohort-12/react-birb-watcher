d4e90d3382d4c17ac2508e965196a92692f99d6b
Luke Lancaster <luketlancaster@gmail.com>
2020-09-02 10:50:41 -0500

    Finish up work on create birb form

--- src/components/pages/NewBirb/NewBirb.js
+++ src/components/pages/NewBirb/NewBirb.js
@@ -27,11 +27,40 @@ class NewBirb extends React.Component {
   changeTypeEvent = (e) => {
     e.preventDefault();
     this.setState({ type: e.target.value });
+  };
+
+  changeColorEvent = (e) => {
+    e.preventDefault();
+    this.setState({ color: e.target.value });
+  }
+
+  changeSizeEvent = (e) => {
+    e.preventDefault();
+    this.setState({ size: e.target.value });
+  }
+
+  changeAltColor = (e) => {
+    e.preventDefault();
+    this.setState({ altColor: e.target.value });
+  }
+
+  changeLocation = (e) => {
+    e.preventDefault();
+    this.setState({ location: e.target.value });
+  }
+
+  changeNotesEvent = (e) => {
+    e.preventDefault();
+    this.setState({ notes: e.target.value });
+  }
+
+  changeWasSleepingEvent = (e) => {
+    this.setState({ wasSleeping: e.target.checked });
   }
 
   seenAtEvent = (seenAt) => {
     this.setState({ seenAt });
-  }
+  };
 
   saveBirb = (e) => {
     e.preventDefault();
@@ -39,22 +68,42 @@ class NewBirb extends React.Component {
     // create new birb object
     // pass that to a data function
     // do something on save?
-    const keysIWant = ['type', 'color', 'size', 'seenAt', 'altColor', 'wasSleeping', 'location', 'notes'];
+    const keysIWant = [
+      'type',
+      'color',
+      'size',
+      'seenAt',
+      'altColor',
+      'wasSleeping',
+      'location',
+      'notes',
+    ];
 
     const newBirb = _.pick(this.state, keysIWant);
     newBirb.uid = authData.getUid();
 
-    birbsData.createBirb(newBirb)
+    birbsData
+      .createBirb(newBirb)
       .then((res) => {
         this.props.history.push(`/birbs/${res.data.name}`);
       })
       .catch((err) => console.error('new birb broke', err));
-  }
+  };
 
   render() {
-    const { type, seenAt } = this.state;
+    const {
+      type,
+      color,
+      size,
+      seenAt,
+      altColor,
+      wasSleeping,
+      location,
+      notes,
+    } = this.state;
+
     return (
-      <div className="NewBirb">
+      <div className="NewBirb col-12">
         <h1>NewBirb</h1>
         <form className="col-6 offset-3">
           <div className="form-group">
@@ -69,14 +118,83 @@ class NewBirb extends React.Component {
             />
           </div>
           <div className="form-group">
-            <label htmlFor="birbSeenAt">Seen At: </label>
+            <label htmlFor="birbColor">Color</label>
+            <input
+              type="text"
+              className="form-control"
+              id="birbColor"
+              placeholder="Enter Birb Color"
+              value={color}
+              onChange={this.changeColorEvent}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="birbSize">Size</label>
+            <input
+              type="text"
+              className="form-control"
+              id="birbSize"
+              placeholder="Enter Birb Size"
+              value={size}
+              onChange={this.changeSizeEvent}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="birbAltColor">Alt Color</label>
+            <input
+              type="text"
+              className="form-control"
+              id="birbAltColor"
+              placeholder="Enter Birb Alt Color"
+              value={altColor}
+              onChange={this.changeAltColor}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="birbLocation">Location</label>
+            <input
+              type="text"
+              className="form-control"
+              id="birbLocation"
+              placeholder="Enter Birb Location"
+              value={location}
+              onChange={this.changeLocation}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="birbNotes">Notes</label>
+            <input
+              type="textarea"
+              className="form-control"
+              id="birbNotes"
+              placeholder="Enter Birb Notes"
+              value={notes}
+              onChange={this.changeNotesEvent}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="birbWasSleeping">Was Sleeping</label>
+            <input
+              type="checkbox"
+              className="form-control"
+              id="birbWasSleeping"
+              checked={wasSleeping}
+              onChange={this.changeWasSleepingEvent}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="birbSeenAt" className="mr-2">
+              Seen At:{' '}
+            </label>
             <DatePicker
               selected={seenAt}
               onChange={this.seenAtEvent}
               showTimeSelect
             />
           </div>
-          <button className="btn btn-warning" onClick={this.saveBirb}>Save Birb</button>
+          <button className="btn btn-warning" onClick={this.saveBirb}>
+            Save Birb
+          </button>
         </form>
       </div>
     );
